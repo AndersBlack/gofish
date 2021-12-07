656db542f16954f178e7168e5194bd9a17064991
Anders Black Larsen <ablack@pop-os.localdomain>
2021-12-07 15:34:46 +0100

    trying to fix

--- src/main/java/com/example/demo/Scraper.py
+++ src/main/java/com/example/demo/Scraper.py
@@ -1,5 +1,6 @@
 import requests
 import re
+import csv
 from bs4 import BeautifulSoup
 
 class Fish:
@@ -29,11 +30,11 @@ def fishScrab():
 
     for fish_elem in fish_elems:
         allTds = fish_elem.find_all('td')
-        fishName = allTds[0].find('i').getText();
+        fishName = allTds[0].find('i').getText().strip();
         aTag = allTds[0].find('a', href=True);
         fishLink = aTag['href'];
-        country = allTds[1].getText();
-        local = allTds[2].getText();
+        country = allTds[1].getText().strip().replace(" ","");
+        local = allTds[2].getText().strip().replace(" ","");
         newFish = Fish(fishName, country, local, fishLink);
         fishList.append(newFish);
 
@@ -53,10 +54,10 @@ def perFishScrab(fish):
 
 
     for tr_elem in tr_elems:
-        fish.habitatType = tr_elems[5].find_all('td')[1].getText();
-        fish.waterType = tr_elems[6].find_all('td')[1].getText();
-        fish.waterDepth = tr_elems[7].find_all('td')[1].getText();
-        fish.abundance = tr_elems[18].find_all('td')[1].getText();
+        fish.habitatType = tr_elems[5].find_all('td')[1].getText().strip().replace(" ","");
+        fish.waterType = tr_elems[6].find_all('td')[1].getText().strip().replace(" ","");
+        fish.waterDepth = tr_elems[7].find_all('td')[1].getText().strip().replace(" ","").replace("\t","");
+        fish.abundance = tr_elems[18].find_all('td')[1].getText().strip();
         latArray = re.findall("([0-9]*[.][0-9]*)",tr_elems[8].find_all('td')[1].getText());
         longArray = re.findall("([0-9]*[.][0-9]*)",tr_elems[9].find_all('td')[1].getText());
         if len(latArray) > 0:
@@ -64,9 +65,17 @@ def perFishScrab(fish):
             fish.long = longArray
 
 
+
 for fish in fishList:
     perFishScrab(fish);
 
+f = open('fish.csv','w');
+writer = csv.writer(f)
+
+writer.writerow(['Fish Name','Country','Location','Habitat','Water Type','Typical Depth','Abundance','Latitude','Longitude'])
+
 for fish in fishList:
     if hasattr(fish, 'lat'):
-        print("Latitude: " + fish.lat[len(fish.lat) - 1] + " Longitude: " + fish.long[len(fish.long) - 1]);
+        writer.writerow([fish.name,fish.country,fish.location,fish.habitatType,fish.waterType,fish.waterDepth,fish.abundance,fish.lat[len(fish.lat) - 1],fish.long[len(fish.long) - 1]])
+
+f.close()
