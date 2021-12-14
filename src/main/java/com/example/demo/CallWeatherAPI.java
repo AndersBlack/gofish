91de3e9ad522a93053ed70aca6c567d846efcd5d
ajess19 <ajess19@student.sdu.dk>
2021-12-14 10:58:01 +0100

    Fixed weatherhiccups

--- src/main/java/com/example/demo/CallWeatherAPI.java
+++ src/main/java/com/example/demo/CallWeatherAPI.java
@@ -37,7 +37,8 @@ public class CallWeatherAPI{
 
     URIBuilder builder = new URIBuilder(requestBuilder.toString());
     builder.setParameter("unitGroup",unitGroup)
-           .setParameter("key",apiKey);
+           .setParameter("key",apiKey)
+           .setParameter("include","days");
 
     HttpGet get = new HttpGet(builder.build());
 
@@ -68,34 +69,35 @@ public class CallWeatherAPI{
 
   // Parse the data from our raw results and mine the data we need for our app
   private static Weather parseWeatherInfoJson(String rawResult) {
+    if(rawResult == null || rawResult.isEmpty()){
+      System.out.printf("No raw data%n");
+			return new Weather();
+		}
 
 		JSONObject weatherInfo = new JSONObject(rawResult);
 
 		ZoneId zoneId=ZoneId.of(weatherInfo.getString("timezone"));
 
     String[] preciptype1 = null;
-    String[] preciptype2 = null;
 
 
 		JSONArray values=weatherInfo.getJSONArray("days");
     JSONObject day1 = values.getJSONObject(0);
-    JSONObject day2 = values.getJSONObject(1);
+    String strday1 = day1.toString();
+
     double temp1 = day1.getDouble("temp");
-    double temp2 = day2.getDouble("temp");
+
     double feelslike1 = day1.getDouble("feelslike");
-    double feelslike2 = day2.getDouble("feelslike");
+
     double precip1 = day1.getDouble("precip");
-    double precip2 = day2.getDouble("precip");
+
     double precipprob1 = day1.getDouble("precipprob");
-    double precipprob2 = day2.getDouble("precipprob");
+
     String sunrise1 = day1.getString("sunrise");
-    String sunrise2 = day2.getString("sunrise");
+
     String sunset1 = day1.getString("sunset");
-    String sunset2 = day2.getString("sunset");
 
     Object day1JSON = day1.get("preciptype");
-    Object day2JSON = day2.get("preciptype");
-
     if(day1JSON instanceof JSONArray){
       JSONArray precipJSON1 = day1.getJSONArray("preciptype");
       preciptype1 = new String[precipJSON1.length()];
@@ -103,22 +105,13 @@ public class CallWeatherAPI{
         preciptype1[i] = precipJSON1.getString(i);
       }
     } else {
+      preciptype1 = new String[1];
       preciptype1[0] = "Unknown";
     }
 
-    if(day2JSON instanceof JSONArray){
-      JSONArray precipJSON2 = day2.getJSONArray("preciptype");
-      preciptype2 = new String[precipJSON2.length()];
-      for(int i = 0; i < precipJSON2.length(); i++){
-        preciptype2[i] = precipJSON2.getString(i);
-      }
-    } else {
-      preciptype2[0] = "Unknown";
-    }
-
     String date1 = ZonedDateTime.ofInstant(Instant.ofEpochSecond(day1.getLong("datetimeEpoch")), zoneId).format(DateTimeFormatter.ISO_LOCAL_DATE);
-    String date2 = ZonedDateTime.ofInstant(Instant.ofEpochSecond(day2.getLong("datetimeEpoch")), zoneId).format(DateTimeFormatter.ISO_LOCAL_DATE);
-    return new Weather(temp1,temp2,feelslike1,feelslike2,precip1,precip2,precipprob1,precipprob2,sunrise1,sunrise2,sunset1,sunset2,preciptype1,preciptype2,date1,date2);
+
+    return new Weather(temp1,feelslike1,precip1,precipprob1,sunrise1,sunset1,preciptype1,date1);
 
 	}
 
--- src/main/java/com/example/demo/IndexController.java
+++ src/main/java/com/example/demo/IndexController.java
@@ -45,50 +45,39 @@ public class IndexController {
     public String testme(Model model, @RequestParam(value = "latitude") String latitude, @RequestParam(value = "longitude") String longitude, RedirectAttributes attrs){
 
        Weather weatherData = null;
-       String[] weatherDataOut = new String[16];
+       String[] weatherDataOut = new String[8];
        String res = "";
-       Weather weatherObject = null;
 
        System.out.println("Recieved a click here on latitude: " + latitude + " and longitude: " + longitude);
 
        // Lav dine test her min dude.
        try{
-       weatherData = CallWeatherAPI.getWeather(latitude + "," + longitude);
-       weatherDataOut[0] = Weather.getDate1(weatherData);
-       weatherDataOut[1] = "" + Weather.getTemp1(weatherData);
-       weatherDataOut[2] = "" + Weather.getFeelslike1(weatherData);
-       weatherDataOut[3] = "" + Weather.getPrecip1(weatherData);
-       weatherDataOut[4] = "" + Weather.getPrecipprob1(weatherData);
-       if(Weather.getPreciptype1(weatherData).length != 0){
-        res = Weather.getPreciptype1(weatherData)[0];
-        for(int i = 1; i < Weather.getPreciptype1(weatherData).length; i++){
-          res = res + "," + Weather.getPreciptype1(weatherData)[i];
-        }
-       }
-       else {
-         res = "";
-       }
-       weatherDataOut[5] = res;
-       res = "";
-       weatherDataOut[6] = Weather.getSunset1(weatherData);
-       weatherDataOut[7] = Weather.getSunrise1(weatherData);
-       weatherDataOut[8] = Weather.getDate2(weatherData);
-       weatherDataOut[9] = "" + Weather.getTemp2(weatherData);
-       weatherDataOut[10] = "" + Weather.getFeelslike2(weatherData);
-       weatherDataOut[11] = "" + Weather.getPrecip2(weatherData);
-       weatherDataOut[12] = "" + Weather.getPrecipprob2(weatherData);
-       if(Weather.getPreciptype2(weatherData).length != 0){
-        res = Weather.getPreciptype2(weatherData)[0];
-        for(int i = 1; i < Weather.getPreciptype2(weatherData).length; i++){
-          res = res + "," + Weather.getPreciptype2(weatherData)[i];
-        }
-       }
-       else {
-         res = "";
-       }
-       weatherDataOut[13] = res;
-       weatherDataOut[14] = Weather.getSunset2(weatherData);
-       weatherDataOut[15] = Weather.getSunrise2(weatherData);
+         weatherData = CallWeatherAPI.getWeather(latitude + "," + longitude);
+
+         weatherDataOut[0] = Weather.getDate1(weatherData);
+
+         weatherDataOut[1] = "" + Weather.getTemp1(weatherData);
+
+         weatherDataOut[2] = "" + Weather.getFeelslike1(weatherData);
+
+         weatherDataOut[3] = "" + Weather.getPrecip1(weatherData);
+
+         weatherDataOut[4] = "" + Weather.getPrecipprob1(weatherData);
+
+         if(Weather.getPreciptype1(weatherData).length != 0){
+          res = Weather.getPreciptype1(weatherData)[0];
+          for(int i = 1; i < Weather.getPreciptype1(weatherData).length; i++){
+            res = res + "," + Weather.getPreciptype1(weatherData)[i];
+          }
+         }
+         else {
+           res = "";
+         }
+         weatherDataOut[5] = res;
+
+         weatherDataOut[6] = Weather.getSunset1(weatherData);
+
+         weatherDataOut[7] = Weather.getSunrise1(weatherData);
 
        } catch (Exception e){
          System.out.println("Error: " + e);
